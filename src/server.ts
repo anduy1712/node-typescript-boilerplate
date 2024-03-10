dcadbd2b6a17126b71fd16031af29d54b679a3e1
DuyAn <duy-an.phan-tran@ekino.com>
2024-03-10 11:21:55 +0700

    first commit

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+const HOST = 'localhost'
+const PORT = 3000
+
+app.get('/', function (req: Request, res: Response) {
+  res.send('Hello World 3')
+})
+
+app.listen(PORT, HOST, () => {
+  console.log('hello 2')
+})
+
+// console.log("test ");
+
+function test(): void {
+  console.log(process.env.TEST_VALUE)
+}
+
+test()
